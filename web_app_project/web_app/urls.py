7fcbc35eb3a0ebafee5c63b6034583c28de26222
Morakinyo Joseph <jmoraks03@gmail.com>
2022-05-17 20:45:33 +0100

    Second commit

--- web_app_project/web_app/urls.py
+++ web_app_project/web_app/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('homepage/', views.homepage, name='homepage'),
     path('register/', views.register, name='register'),
-    path('login', views.login, name='login'),
-    path('logout', views.logging_out, name='logging_out'),
+    path('logging_in', views.logging_in, name='logging_in'),
+    path('logging_out', views.logging_out, name='logging_out'),
+    path('post/<str:pk>', views.post, name='post'),
+    path('space', views.space, name='space'),
 ]
--- web_app_project/web_app/views.py
+++ web_app_project/web_app/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render, redirect
 from .models import Blog
 from django.contrib.auth.models import User
 from django.contrib import messages
-from django.contrib.auth import authenticate, logout
+from django.contrib.auth import authenticate, logout, login
 
 
 def index(request):
     if request.method == 'POST':
         if request.POST['login']:
-            return redirect('login')
+            return redirect('logging_in')
     else:
         return render(request, 'index.html', {})
 
@@ -37,7 +37,7 @@ def register(request):
             else:
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
-                return redirect('login')
+                return redirect('logging_in')
 
         else:
             messages.info(request, 'Passwords do not match!')
@@ -47,7 +47,7 @@ def register(request):
         return render(request, 'register.html')
 
 
-def login(request):
+def logging_in(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -55,14 +55,34 @@ def login(request):
         user = authenticate(username=username, password=password)
 
         if user is not None:
+            login(request, user=user)
             return redirect('homepage')
         else:
             messages.info(request, 'Invalid Username or Password!')
-            return redirect('login')
+            return redirect('logging_in')
     else:
         return render(request, 'login.html', {})
 
 
 def logging_out(request):
     logout(request)
-    return render(request, 'index.html')
+    return render(request, 'homepage.html')
+
+
+def post(request, pk):
+    blog = Blog.objects.get(id=pk)
+    return render(request, 'post.html', {"blog": blog})
+
+
+def space(request):
+    if request.method == 'POST':
+        headline = request.POST['headline']
+        content = request.POST['content']
+
+        story = Blog.objects.create(headline=headline, content=content)
+        story.save()
+        return redirect('homepage')
+
+    else:
+        return render(request, 'space.html', {})
+
